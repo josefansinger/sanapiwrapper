8d5d5cb2c512e9cbd6b53db52a419b67f86fe3eb
josefansinger <josef.ansinger@gmx.de>
2020-05-15 17:07:33 +0200

    add amount_in_top_holders test

--- tests/testthat/test-santimentMetric.R
+++ tests/testthat/test-santimentMetric.R
@@ -11,11 +11,20 @@ test_that("get development activity for ethereum", {
 })
 
 test_that("get daily active addresses for ethereum", {
-
+  
   data <- santimentMetric('daily_active_addresses', 'ethereum')
-
+  
   expect_equal(data$daily_active_addresses[which(data$date == '2019-01-01')], 197884)
   expect_equal(data$daily_active_addresses[which(data$date == '2019-07-01')], 288905)
   expect_equal(data$daily_active_addresses[which(data$date == '2020-01-01')], 160388)
+  
+})
+
+test_that("get amount in top-10 holders for ethereum", {
+  
+  data <- santimentMetric('amount_in_top_holders', 'ethereum', '2020-04-01', '2020-05-01', selector_option = 'holdersCount:10')
+  
+  expect_equal(data$amount_in_top_holders[which(data$date == '2020-04-01')], 15811960)
+  expect_equal(data$amount_in_top_holders[which(data$date == '2020-04-11')], 16480552)
 
 })
